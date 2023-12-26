998ecad0b6d363e0db14c66d67878119175f6d95
Krish Goel <krishgoel20mar@gmail.com>
2023-12-26 22:48:58 +0530

    Changes made.

--- Front-end/index.js
+++ Front-end/index.js
@@ -12,3 +12,47 @@ login.addEventListener("click", () => {
     slider.classList.remove("moveslider");
     formSection.classList.remove("form-section-move");
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Login Button
+    var loginButton = document.querySelector('.login-box .clkbtn');
+    if (loginButton) 
+    {
+        loginButton.addEventListener('click', function () {
+            var emailInput = document.querySelector('.login-box .email');
+            var passwordInput = document.querySelector('.login-box .password');
+
+            if (!validateEmail(emailInput.value) || passwordInput.value.trim() === '') 
+            {
+                alert("Please enter a valid E-mail and Password.");
+                return;
+            }
+            location.reload();
+        });
+    }
+    
+	// Signup Button 
+    var signupButton = document.querySelector('.signup-box .clkbtn');
+    if (signupButton) 
+    {
+        signupButton.addEventListener('click', function () {
+            var nameInput = document.querySelector('.signup-box .name');
+            var emailInput = document.querySelector('.signup-box .email');
+            var passwordInput = document.querySelector('.signup-box .password');
+            var confirmPasswordInput = document.querySelector('.signup-box .password-confirm');
+
+            if (nameInput.value.trim() === '' || !validateEmail(emailInput.value) || passwordInput.value.trim() === '' || passwordInput.value !== confirmPasswordInput.value) 
+            {
+                alert("Please enter valid information and make sure the passwords match.");
+                return;
+            }
+            location.reload();
+        });
+    }
+
+    function validateEmail(email) 
+    {
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+});
